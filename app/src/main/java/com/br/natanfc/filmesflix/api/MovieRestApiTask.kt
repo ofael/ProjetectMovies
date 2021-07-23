247f64871e56df5fdd6f8156ba73647db6429bba
ofael <fael_assis@hotmail.com>
2021-07-23 11:51:42 -0300

    make setup and search webservice of project! creating interface MovieApi.kt and class MovieRestApiTask.kt

---
+++ app/src/main/java/com/br/natanfc/filmesflix/api/MovieRestApiTask.kt
@@ -0,0 +1,21 @@
+package com.br.natanfc.filmesflix.api
+
+import com.br.natanfc.filmesflix.model.Movie
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+
+class MovieRestApiTask {
+
+    companion object{
+        const val BASE_URL = "https://raw.githubusercontent.com"
+    }
+
+    private fun movieProvider(): Retrofit = Retrofit.Builder().
+            baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+    fun retrofitApi() : MovieApi = movieProvider().create(MovieApi::class.java)
+
+}
